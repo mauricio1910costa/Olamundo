a0973481ec499bb6a67f973b494c5529620546a5
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-05 00:30:20 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Olá usuário, eu me chamo Maurício!");
+        }
+    }
+}
